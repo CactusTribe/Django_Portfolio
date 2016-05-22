8416053e0fb94b42ac0254374e0fd4693f797a38
CactusTribe <lefrancjoaquim@gmail.com>
2016-05-22 18:51:55 +0200

    Début de traduction EN

--- home/urls.py
+++ home/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('home.views',
 	url(r'^$', 'home'),
+	url(r'^(?P<langue>(fr|en))/$', 'home'),
 )
--- home/views.py
+++ home/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from home.models import Categorie, Project, Image, Resource, IconFile
 
-def home(request):
+def home(request, langue="fr"):
 	projects = Project.objects.all().order_by("date")
 	images = Image.objects.all()
 	resources = Resource.objects.all()
 	iconsfiles = IconFile.objects.all()
 	categories = Categorie.objects.all()
-	
+
 	return render(request, 'index.html', locals())
