7ed7b18c14db2c42f385b9ba5e00e26f81b7576d
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-05 13:40:51 +0100

    Mise en place du mail

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404
 from home.models import Categorie, Project, Image, Resource, IconFile
+from django.core.mail import send_mail
 
 import subprocess
 
@@ -16,4 +17,18 @@ def home(request, langue="fr"):
 	else:
 		subprocess.Popen(["python3","/home/pi/pyprojects/Divers/LED_RGB.py", "215", "20", "0"])
 
+
+	name = request.GET.get('name')
+	email = request.GET.get('email')
+	message = request.GET.get('message')
+
+	if name != None and email != None and message != None:
+		send_mail(
+		    '[CONTACT]',
+		    message,
+		    email,
+		    ['lefrancjoaquim@gmail.com'],
+		    fail_silently=False,
+		)
+		
 	return render(request, 'index.html', locals())
