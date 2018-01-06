003a724da75edef48526322d779cb724dc70285f
CactusTribe <lefrancjoaquim@gmail.com>
2018-01-06 13:04:01 +0100

    Affichage des resources seulement si elles existent

--- home/templatetags/home_extras.py
+++ home/templatetags/home_extras.py
@@ -5,3 +5,7 @@ register = template.Library()
 @register.filter
 def getImagesProject(images, id_project):
 	return images.filter(project=id_project)
+
+@register.filter
+def getResourcesProject(resources, id_project):
+	return resources.filter(project=id_project)
